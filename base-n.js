534542e21cf189f4e6442a12718e75f2aac0d78d
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-08 22:59:27 -0400

    init commit

---
+++ base-n.js
@@ -0,0 +1,17 @@
+var numberDigits = [];
+var alphaDigits = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+for (let i = 1; i < 10; i++){
+	numberDigits.push(`${i}`);
+}
+var allDigits = numberDigits.concat(alphaDigits);
+
+var base = 2;
+
+baseTenNumber = 96;
+
+var numOK = true;
+
+while(numOK){
+	let i = 0;
+
+}

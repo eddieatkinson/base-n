dd01d63d8723554c793eeb35bdba954d597cace0
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-09 00:37:35 -0400

    Base n

--- base-n.js
+++ base-n.js
@@ -1,17 +1,41 @@
 var numberDigits = [];
 var alphaDigits = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-for (let i = 1; i < 10; i++){
+for (let i = 0; i < 10; i++){
 	numberDigits.push(`${i}`);
 }
 var allDigits = numberDigits.concat(alphaDigits);
 
 var base = 2;
 
-baseTenNumber = 96;
+var finalNum = "";
+
+var baseTenNumber = 95;
 
 var numOK = true;
+var digitsForBase = []
 
-while(numOK){
-	let i = 0;
-
+for(let i = 0; i < base; i++){
+	digitsForBase.push(i);
 }
+var exp = 0;
+// var expCounter = 1;
+var numLeft = baseTenNumber;
+var numTimes;
+while(numLeft > 0){
+	while(numOK){
+		if(Math.pow(base, exp) > numLeft){
+			exp--;
+			numOK = false;
+		}else{
+			exp++;
+		}
+	}
+	numTimes = Math.floor(numLeft / Math.pow(base, exp));
+	console.log(numTimes);
+	finalNum += allDigits[numTimes];
+	numLeft -= Math.pow(base, exp);
+	console.log(numLeft);
+	numOK = true;
+}
+console.log(finalNum);
+
